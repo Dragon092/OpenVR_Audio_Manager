f02524d83a43fae59dd36953b2b769078d974326
Kevin Birke <mail@kevinbirke.de>
2020-11-19 10:34:56 +0100

    Add controller to save and load settings

--- audiostatecontroller.cpp
+++ audiostatecontroller.cpp
@@ -1,6 +1,66 @@
 #include "audiostatecontroller.h"
 
+#include <QSettings>
+#include <QDebug>
+
+AudioStateController *s_pSharedAudioStateController = NULL;
+
+AudioStateController *AudioStateController::SharedInstance()
+{
+    if ( !s_pSharedAudioStateController )
+    {
+        s_pSharedAudioStateController = new AudioStateController();
+    }
+    return s_pSharedAudioStateController;
+}
+
 AudioStateController::AudioStateController()
 {
+    QSettings settings;
+    qDebug() << "Settings fileName: " << settings.fileName();
+    loadSettings();
+}
 
+void AudioStateController::loadSettings()
+{
+    qDebug() << "Loading settings...";
+
+    QSettings settings;
+    m_prefs.output_wearinghmd = settings.value("output_wearinghmd").toString();
+    m_prefs.output_notwearinghmd = settings.value("output_notwearinghmd").toString();
+    m_prefs.input_wearinghmd = settings.value("input_wearinghmd").toString();
+    m_prefs.input_notwearinghmd = settings.value("input_notwearinghmd").toString();
+
+    //qDebug() << "default_output_default: " << m_prefs.default_output_default;
+    //qDebug() << "default_output_communication: " << m_prefs.default_output_communication;
+}
+
+void AudioStateController::saveSettings()
+{
+    qDebug() << "Saving settings...";
+
+    QSettings settings;
+    settings.setValue("output_wearinghmd", m_prefs.output_wearinghmd);
+    settings.setValue("output_notwearinghmd", m_prefs.output_notwearinghmd);
+    settings.setValue("input_wearinghmd", m_prefs.input_wearinghmd);
+    settings.setValue("input_notwearinghmd", m_prefs.input_notwearinghmd);
+}
+
+void AudioStateController::setDevice(AudioDeviceDirection direction, std::string deviceID)
+{
+    std::string default_default = GetDefaultAudioDeviceID(direction, AudioDeviceRole::DEFAULT);
+    std::string default_communication = GetDefaultAudioDeviceID(direction, AudioDeviceRole::COMMUNICATION);
+
+    qDebug() << "default input default:           " << default_default.data();
+    qDebug() << "default input communication:     " << default_communication.data();
+    qDebug() << "new DeviceID:                    " << deviceID.data();
+    qDebug() << "-----";
+
+    if(deviceID != default_default){
+        SetDefaultAudioDeviceID(direction, AudioDeviceRole::DEFAULT, deviceID);
+    }
+
+    if(deviceID != default_communication){
+        SetDefaultAudioDeviceID(direction, AudioDeviceRole::COMMUNICATION, deviceID);
+    }
 }
--- audiostatecontroller.h
+++ audiostatecontroller.h
@@ -1,11 +1,31 @@
 #ifndef AUDIOSTATECONTROLLER_H
 #define AUDIOSTATECONTROLLER_H
 
+#include <QString>
+#include <AudioFunctions.h>
+
+struct SPreferences
+{
+    QString output_wearinghmd;
+    QString output_notwearinghmd;
+
+    QString input_wearinghmd;
+    QString input_notwearinghmd;
+};
 
 class AudioStateController
 {
 public:
+    static AudioStateController *SharedInstance();
+public:
     AudioStateController();
+
+    void loadSettings();
+    void saveSettings();
+
+    void setDevice(AudioDeviceDirection direction, std::string deviceID);
+
+    SPreferences m_prefs;
 };
 
 #endif // AUDIOSTATECONTROLLER_H
